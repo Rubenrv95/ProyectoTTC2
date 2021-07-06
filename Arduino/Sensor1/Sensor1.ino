ad830de116471500378281ac1bb770c1a7b29ecc
Matias Pizarro <pinkystylee98@gmail.com>
2021-07-06 13:11:36 -0400

    Ta listo
    creo

--- Arduino/Sensor1/Sensor1.ino
+++ Arduino/Sensor1/Sensor1.ino
@@ -1,6 +1,9 @@
 #define sensor1 3
 #define sensor2 7
 
+int sensor1Anterior = -1;
+int sensor2Anterior = -1;
+
 void setup(){
  Serial.begin (9600);
   pinMode (sensor1 , INPUT);
@@ -11,22 +14,17 @@ void loop(){
   int rain = digitalRead(sensor1);
   int rain2 = digitalRead(sensor2);
 
-  if(rain==1) {
-    Serial.println("Esta lloviendo en Curico");
-    delay(500);
+  if(rain != sensor1Anterior){
+    Serial.print("Curico ");
+    Serial.println(rain);
+    sensor1Anterior = rain;
   }
-  else if(rain==0) {
-    Serial.println("No llueve en Curico");
-    delay(500);
+  delay(500);
+  if(rain2 != sensor2Anterior){
+    Serial.print("Talca ");
+    Serial.println(rain2);
+    sensor2Anterior = rain2;
   }
-
-  if(rain2==1) {
-    Serial.println("Esta lloviendo en Talca");
-    delay(500);
-  }
-  else if(rain2==0) {
-    Serial.println("No llueve en Talca");
-    delay(500);
-  }
+  delay(500);
 
 }
--- Python/Conexion.py
+++ Python/Conexion.py
@@ -1,8 +1,44 @@
-import serial
+import sys, serial
+from Adafruit_IO import MQTTClient
 
+ADAFRUIT_IO_USERNAME = "MatiasPizarro"
+ADAFRUIT_IO_KEY = "aio_hXDO93A4VX7u1PWMRs8z6FUIrHyj"
 ser = serial.Serial('COM2',9600)
+
+def connected(client):
+    client.subscribe('Curico')
+    client.subscribe('Talca')
+
+def disconnected(client):
+    sys.exit(1)
+
+def message(client,feed_id,payload):
+    if(payload[0] == "1"):
+        print('Esta lloviendo en la cuidad de {0}'.format(feed_id))
+    elif (payload[0] == "0"):
+        print('No esta lloviendo en la cuidad de {0}'.format(feed_id))
+    else:
+        print("Error al recibir el dato")
+
+
+client = MQTTClient(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+
+client.connect()
+client.loop_background()   
 
 while True:
     dato = ser.readline()
     dato = format(dato.decode('utf-8'))
-    print(dato)
+    if(dato != ""):
+        separado = dato.split(" ")
+        if(separado[0] == "Curico"):
+            client.publish('Curico',separado[1])
+        elif(separado[0] == "Talca"):
+            client.publish('Talca', separado[1])
+        print(dato)
+    else:
+        print("no hay dato")
+
